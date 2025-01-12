aca88c7351d3feacd31f1237252b0cf293e233bc
Suytry Sok <soksoytry2000@gmail.com>
2025-01-12 21:49:39 +0700

    init_project

---
+++ target/generated-sources/annotations/com/java/school/online_video_training/mapper/CategoryMapperImpl.java
@@ -0,0 +1,26 @@
+package com.java.school.online_video_training.mapper;
+
+import com.java.school.online_video_training.dto.CategoryDTO;
+import com.java.school.online_video_training.entity.Category;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-12T20:59:10+0700",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240820-0604, environment: Java 21.0.4 (Eclipse Adoptium)"
+)
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public Category toCategory(CategoryDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        category.setName( dto.getName() );
+
+        return category;
+    }
+}
