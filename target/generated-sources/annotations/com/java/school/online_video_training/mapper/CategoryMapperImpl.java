dacf2d99ca339fa5d26ec3472b22fd949950a625
Suytry Sok <soksoytry2000@gmail.com>
2025-07-22 23:13:26 +0700

    End_AllSt

--- target/generated-sources/annotations/com/java/school/online_video_training/mapper/CategoryMapperImpl.java
+++ target/generated-sources/annotations/com/java/school/online_video_training/mapper/CategoryMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-18T17:10:05+0700",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
+    date = "2025-07-22T22:55:38+0700",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240820-0604, environment: Java 21.0.4 (Eclipse Adoptium)"
 )
 @Component
 public class CategoryMapperImpl implements CategoryMapper {
